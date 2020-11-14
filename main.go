77b135544a6a2a37153790cf09bfb96860bf2b34
Mervin Hemaraju <mervinhemaraju16@gmail.com>
2020-11-14 20:14:16 +0400

    - setup gin engine - setup gin hot reload - setup routes for all services and one service - setup json file reading

--- main.go
+++ main.go
@@ -1,13 +1,84 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+var singleJSONTest string = `{
+	"identifier": "security-police-direct-1",
+	"name": "Police Direct Line 1",
+	"type": "SECURITY",
+	"icon": "https://img.icons8.com/fluent/100/000000/policeman-male.png",
+	"number": 999
+}`
+
+var multipleJSONTest string = `[
+	{
+		"identifier": "security-police-direct-1",
+		"name": "Police Direct Line 1",
+		"type": "SECURITY",
+		"icon": "https://img.icons8.com/fluent/100/000000/policeman-male.png",
+		"number": 999
+	},
+	{
+		"identifier": "health-samu",
+		"name": "Samu",
+		"type": "HEALTH",
+		"icon": "https://img.icons8.com/bubbles/100/000000/ambulance.png",
+		"number": 114
+	}
+]`
+
+type serviceStruct struct {
+	Identifier string `json:"identifier"`
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	Icon       string `json:"icon"`
+	Number     int    `json:"number"`
+}
+
+func oneService(c *gin.Context) {
+
+	//language := c.Param("language")
+
+	var service serviceStruct
+
+	json.Unmarshal([]byte(singleJSONTest), &service)
+
+	c.JSON(200, service)
+
+	//fmt.Printf("%T\n", singleJSONTest)
+}
+
+func allServices(c *gin.Context) {
+
+	//language := c.Param("language")
+
+	file, _ := ioutil.ReadFile("data/services_en.json")
+
+	var keys []serviceStruct
+
+	json.Unmarshal([]byte(file), &keys)
+
+	c.JSON(200, gin.H{"services": keys, "message": "", "success": true})
+}
+
+func setupRoutes(engine *gin.Engine) {
+	engine.GET("/mesg/:language/services", allServices)
+	engine.GET("/mesg/:language/service/:id", oneService)
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	// Creates the Gin Engine
+	engine := gin.Default()
+
+	// Setup the API Routes
+	setupRoutes(engine)
+
+	// Run the engine
+	engine.Run()
 }
